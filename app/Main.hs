779507b0c5eb56e854ebadecb5cb550f7fa84c48
kevenalex <keven.alex.barbosa.santos@ccc.ufcg.edu.br>
2024-08-16 22:43:28 -0300

    AFF

--- app/Main.hs
+++ app/Main.hs
@@ -1,24 +1,24 @@
 module Main where
 
-import Models.Grid
-import Models.Ruler
+    import Models.Grid
+    import Models.Ruler
 
-main :: IO ()
-main = do
+    main :: IO ()
+    main = do
 
-    putStrLn "Digite uma sequência de números representando a quantidade de vizinhos para instanciar a regra"
-    putStr "Regra de nascimento: "
-    
-    nascStr <- getLine
-    let nascList = map (\x -> read [x] :: Int) nascStr
+        putStrLn "Digite uma sequência de números representando a quantidade de vizinhos para instanciar a regra"
+        putStr "Regra de nascimento: "
+        
+        nascStr <- getLine
+        let nascList = map (\x -> read [x] :: Int) nascStr
 
-    putStr "Regra de permanênmcia: "
+        putStr "Regra de permanênmcia: "
 
-    stayStr <- getLine
-    let stayList = map (\x -> read [x] :: Int) stayStr
+        stayStr <- getLine
+        let stayList = map (\x -> read [x] :: Int) stayStr
 
 
-    let regra = Ruler nascList stayList
+        let regra = Ruler nascList stayList
 
-    putStrLn ("A sua regra tem a seguinte forma " ++ (show regra))
+        putStrLn ("A sua regra tem a seguinte forma " ++ (show regra))
 
