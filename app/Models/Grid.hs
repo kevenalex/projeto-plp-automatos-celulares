141b2a7b6a9fc95a8028a2e1d5a4ef164e0c39bf
kevenalex <keven.alex.barbosa.santos@ccc.ufcg.edu.br>
2024-08-16 22:44:53 -0300

    "mensagem"

--- app/Models/Grid.hs
+++ app/Models/Grid.hs
@@ -1,22 +1,22 @@
 module Models.Grid where
-import System.IO
-import System.IO.Unsafe (unsafeDupablePerformIO)
-import Data.Matrix
-import Models.Cell
+    import System.IO
+    import System.IO.Unsafe (unsafeDupablePerformIO)
+    import Data.Matrix
+    import Models.Cell
 
-data Grid = Grid {
-    height :: Int,
-    width :: Int,
-    grade :: Matrix Cell
-}
-
-
-gridGenerate :: Int -> Int -> Cell -> Grid
-gridGenerate height width a = Grid height width (matrix height width (\_ -> a))
-
-insertCell :: Grid -> Cell -> (Int, Int) -> Grid
-insertCell grid cell (x,y) = Grid (height grid) (width grid) (setElem cell (x,y) (grade grid))
+    data Grid = Grid {
+        height :: Int,
+        width :: Int,
+        grade :: Matrix Cell
+    }
 
 
--- gridUpdate :: Grid -> Grid
--- gridUpdate = 
+    gridGenerate :: Int -> Int -> Cell -> Grid
+    gridGenerate height width a = Grid height width (matrix height width (\_ -> a))
+
+    insertCell :: Grid -> Cell -> (Int, Int) -> Grid
+    insertCell grid cell (x,y) = Grid (height grid) (width grid) (setElem cell (x,y) (grade grid))
+
+
+    -- gridUpdate :: Grid -> Grid
+    -- gridUpdate = 
