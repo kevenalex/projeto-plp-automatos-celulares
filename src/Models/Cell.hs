554107f4e05f432fa07688123683de42d6695439
leofarias-cpu <l3onardo.almeid4@gmail.com>
2024-08-22 09:36:19 -0300

    implementa jsons pra cell e regra

--- src/Models/Cell.hs
+++ src/Models/Cell.hs
@@ -1,18 +1,26 @@
+{-# LANGUAGE DeriveGeneric, DeriveAnyClass, OverloadedStrings #-}
+-- pro generic, pra não ter que fazer implementação default, pra ler strings como bytecode
+
 module Models.Cell where
 
     import Models.Rule
     import Data.Maybe (isJust)
-         
-    instance Show Cell where
-        show cell = show (name cell)
+    import Data.Aeson
+    import GHC.Generics
+
 
     data Cell =   
         Cell {
         name :: String,
         rule :: Rule,
         color :: String 
-        }
+        } deriving (Generic,ToJSON, FromJSON)
     
+
+    instance Show Cell where
+        show cell = show (name cell)
+
+
     isLive :: Maybe Cell -> Bool
     isLive mCell = isJust mCell
      
--- src/Models/Rule.hs
+++ src/Models/Rule.hs
@@ -1,9 +1,14 @@
+{-# LANGUAGE DeriveGeneric, DeriveAnyClass #-}
+
 module Models.Rule where
+
+    import GHC.Generics
+    import Data.Aeson
 
     data Rule = Rule {
         birth :: [Int],
         stay :: [Int]
-    } 
+    } deriving (Generic, ToJSON, FromJSON)
 
     instance Show Rule where
         show (Rule birth stay)= "B" ++ (concatMap show birth) ++ "/S" ++ (concatMap show stay)
