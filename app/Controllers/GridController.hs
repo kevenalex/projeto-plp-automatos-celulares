bb7a22b425d519d396ef19a11ce3bda8f4cf4f02
José Ramon Severo Alves <joseramonalves@gmail.com>
2024-08-26 12:39:37 -0300

    eba tem uma funcao q roda alguma coisa eba

--- app/Controllers/GridController.hs
+++ app/Controllers/GridController.hs
@@ -7,7 +7,6 @@ module Controllers.GridController where
     import qualified Data.Vector as V
     import Data.List (intercalate)
     import Data.Maybe
-    import Models.Grid
     import Data.Char (intToDigit)
     
 
@@ -19,7 +18,7 @@ module Controllers.GridController where
     -- com mais de 2 casas decimais para não quebrar a formatação
 
     printGridWithNumbers :: [[Maybe Cell]] -> Int ->  IO ()
-    printGridWithNumbers [] _ =  return ()
+    printGridWithNumbers [] n =  return ()
     printGridWithNumbers (x:xs) n = 
         if n == 0 then do
             putStrLn $ "  " ++ buildLineWithNumber (length x)
@@ -32,8 +31,7 @@ module Controllers.GridController where
         where
             nStr = if n < 10 then " " ++ show n else show n
                
-    -- Função que imprime uma Matrix (Maybe Cell) sem os números correspondentes a cada linha e coluna, 
-    -- a mesma recebe uma lista de Maybe Cell, que pode ser facilmente obtida através do
+    -- Função que imprime uma Matrix (Maybe Cell) sem os números correspondentes a cada linha e coluna, a mesma recebe uma lista de Maybe Cell, que pode ser facilmente obtida através do
     -- método gridToLists aplicado a uma Matrix (Maybe Cell). Sempre inicie o método com o n igual a 0, e procure não utilizar uma matriz
     -- com mais de 2 casas decimais para não quebrar a formatação
 
@@ -49,12 +47,38 @@ module Controllers.GridController where
 
     -- Função que retorna a String formatada de cada linha de uma Matrix (Maybe Cell)
     buildLine :: [Maybe Cell] -> String
-    buildLine cellRow = "|" ++ intercalate "|" cells ++ "|"
+    buildLine cellRow = intercalate "" cells
         where 
-            cells = [if isAlive cell then color (fromJust cell) else "⬛" | cell <- cellRow]
+            cells = [if isAlive cell then color (fromJust cell) else "⬛ " | cell <- cellRow]
 
     -- Função que retorna a String formatada dos números correspondentes a cada coluna de uma Matrix (Maybe Cell)
     buildLineWithNumber :: Int -> String
-    buildLineWithNumber n = "|" ++ intercalate "|" list ++ "|"
+    buildLineWithNumber n = intercalate " " list ++ " "
         where list = [if c > 9 then show c else " " ++ show c | c <-[1..n]]
     -- ------------------------------------------------------------------------------------------------
+
+    simulate :: Matrix (Maybe Cell) -> IO()
+    simulate matrix = do
+        -- _ <- system "clear"
+        printGrid (gridToLists matrix) 0
+        putStrLn "digite G para iniciar a geração"
+        putStrLn "digite N para simular só o próximo estágio"
+        putStrLn "digite I para inserir células"
+        option <- getLine
+        actionChooser matrix option
+
+    actionChooser :: Matrix (Maybe Cell) -> String -> IO()
+    actionChooser grid option 
+        | option == "G" = startGeneration grid
+        | option == "N" = nextStep grid
+        | option == "I" = insertion grid
+        | otherwise = simulate grid
+
+    startGeneration :: Matrix (Maybe Cell) -> IO()
+    startGeneration grid = putStrLn "foda-se"
+
+    nextStep :: Matrix (Maybe Cell) -> IO()
+    nextStep grid = putStrLn "foda-se"
+
+    insertion :: Matrix (Maybe Cell) -> IO()
+    insertion grid = putStrLn "gay"
