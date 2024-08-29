5eca7789d2205b78c8f0a6b724eefe0c5eaa1999
Leonardo Almeida Farias <113396827+leofarias-cpu@users.noreply.github.com>
2024-08-29 09:36:47 -0300

    teste de múltiplas células e renomeia AbstractSingletonFactoryBuilderCellController

--- test/conwaysTest.hs
+++ test/conwaysTest.hs
@@ -22,16 +22,16 @@ module Test.Simulation where
                      square 3,
             "Flicker com 2 amigos" ~:
                 gridUpdate (insertCells (square 3) conways [(2,1), (2,2), (2,3)]) ~?= 
-                    insertCells (square 3) conways [(1,2), (2,2), (3,2)]
-
-                     ]
+                    insertCells (square 3) conways [(1,2), (2,2), (3,2)],
+            "Conways e o amigo alto" ~: gridUpdate test3Setup ~?= test3Result
+                    ]
 
 --- testes ---
     test3Setup :: Matrix (Maybe Cell)
-    test3Setup = insertCells (insertCells (square 5) highLife [(2,2), (3,2), (4,2)])
-             conways [(2,4), (3,4), (4,4)] 
+    test3Setup = insertCells (insertCells (square 5) highLife [(2,2), (3,2), (4,2)])conways [(2,4), (3,4), (4,4)] 
     test3Result :: Matrix (Maybe Cell)
     test3Result = insertCells(insertCells (square 5) highLife [(3,1), (3,2), (3,3)]) conways [(3,4), (3,5)]
+
 --- Constantes ---
     conways :: Cell
     conways = Cell "C" (Rule [3] [2,3]) "Verde"
