05c2104cf9e5789c370e3dc44200d4f1d209cfae
Jacobs879 <victor.jacob.oliveira.rodrigues.silva@ccc.ufcg.edu.br>
2024-08-31 03:15:19 -0300

    Mais testes feitos com amor e carinho ♡

--- test/conwaysTest.hs
+++ test/conwaysTest.hs
@@ -38,7 +38,9 @@ module Test.Simulation where
     listaDeTestes =
         TestList [
             testNumOfDeadNeighbors0, testNumOfDeadNeighbors1, testNumOfDeadNeighbors2, testNumOfDeadNeighbors3, 
-            testGridGenerateFromList0, testGridGenerateFromList1, testGridGenerateFromList2, testGridGenerateFromList3
+            testGridGenerateFromList0, testGridGenerateFromList1, testGridGenerateFromList2, testGridGenerateFromList3,
+            testNumOfLiveNeighbors0, testNumOfLiveNeighbors1, testNumOfLiveNeighbors2, testNumOfLiveNeighbors3,
+            testNumOfLiveNeighbors4, testGetCellTrue, testGetCellFalse
         ]
 
 ----------------------------------------- [MAIN: TESTES DO MÓDULO GRID] ------------------------------------------
@@ -65,11 +67,9 @@ module Test.Simulation where
 
     testGridGenerateFromList0 :: Test
     testGridGenerateFromList0 = TestCase $ do
-        let resultGrid = gridGenerateFromList rows cols cellList
         assertEqual "Grid from List 0" expectedGrid resultGrid
         where
-            rows = 2
-            cols = 3
+            resultGrid = gridGenerateFromList 2 3 cellList
             cellList = [Just conways, Just conways, Just conways, Nothing, Nothing, Nothing]
             expectedGrid = 
                 fromLists [
@@ -79,13 +79,10 @@ module Test.Simulation where
 
     testGridGenerateFromList1 :: Test
     testGridGenerateFromList1 = TestCase $ do
-        let resultGrid = gridGenerateFromList rows cols cellList
         assertEqual "Grid from List 1" expectedGrid resultGrid
         where
-            rows = 2
-            cols = 3
-            cellList = 
-                [Nothing, Nothing, Nothing, Nothing, Nothing, Nothing]
+            resultGrid = gridGenerateFromList 2 3 cellList
+            cellList = [Nothing, Nothing, Nothing, Nothing, Nothing, Nothing]
             expectedGrid = 
                 fromLists [
                     [Nothing, Nothing, Nothing], 
@@ -94,11 +91,9 @@ module Test.Simulation where
 
     testGridGenerateFromList2 :: Test
     testGridGenerateFromList2 = TestCase $ do
-        let resultGrid = gridGenerateFromList rows cols cellList
         assertEqual "Grid from List 2" expectedGrid resultGrid
         where
-            rows = 2
-            cols = 3
+            resultGrid = gridGenerateFromList 2 3 cellList
             cellList = [Just conways, Just conways, Just conways, Just conways, Just conways, Just conways]
             expectedGrid = 
                 fromLists [
@@ -108,11 +103,9 @@ module Test.Simulation where
 
     testGridGenerateFromList3 :: Test
     testGridGenerateFromList3 = TestCase $ do
-        let resultGrid = gridGenerateFromList rows cols cellList
         assertEqual "Grid from List 3" expectedGrid resultGrid
         where
-            rows = 3
-            cols = 2
+            resultGrid = gridGenerateFromList 3 2 cellList
             cellList = [Just conways, Nothing, Just conways, Nothing, Just conways, Nothing]
             expectedGrid = 
                 fromLists [
@@ -130,43 +123,99 @@ module Test.Simulation where
 
     testNumOfDeadNeighbors0 :: Test
     testNumOfDeadNeighbors0 = TestCase $ do
-        let grid = square 3
-        let resultNum = numOfDeadNeighbors (x,y) grid
-        assertEqual "Vizinhaça Zumbi 0" expectedGOLNum resultNum
+        assertEqual "Vizinhaça Zumbi 0" expectedNum resultNum
         where
-            x = 2
-            y = 2
-            expectedGOLNum = 8
+            expectedNum = 8
+            resultNum = numOfDeadNeighbors (2,2) (square 3)
 
     testNumOfDeadNeighbors1 :: Test
     testNumOfDeadNeighbors1 = TestCase $ do
-        let grid = square 1
-        let resultNum = numOfDeadNeighbors (x,y) grid
-        assertEqual "Vizinhaça Zumbi 1" expectedGOLNum resultNum
+        assertEqual "Vizinhaça Zumbi 1" expectedNum resultNum
         where
-            x = 1
-            y = 1
-            expectedGOLNum = 8
+            expectedNum = 8
+            resultNum = numOfDeadNeighbors (1,1) (square 1)
 
     testNumOfDeadNeighbors2 :: Test
     testNumOfDeadNeighbors2 = TestCase $ do
-        let grid = insertCells (square 3) conways [(1,2), (2,1), (2,3), (3,2)]
-        let resultNum = numOfDeadNeighbors (x,y) grid
-        assertEqual "Vizinhaça Zumbi 2" expectedGOLNum resultNum
+        assertEqual "Vizinhaça Zumbi 2" expectedNum resultNum
         where
-            x = 2
-            y = 2
-            expectedGOLNum = 4
+            expectedNum = 4
+            grid = insertCells (square 3) conways [(1,2), (2,1), (2,3), (3,2)]
+            resultNum = numOfDeadNeighbors (2,2) grid
 
     testNumOfDeadNeighbors3 :: Test
     testNumOfDeadNeighbors3 = TestCase $ do
-        let preGrid = insertCells (square 3) highLife [(1,1), (1,3), (3,1), (3,3)]
-        let grid = insertCells preGrid conways [(1,2), (2,1), (2,3), (3,2)]
-        let resultNum = numOfDeadNeighbors (x,y) grid
-        assertEqual "Vizinhaça Zumbi 3" expectedGOLNum resultNum
+        assertEqual "Vizinhaça Zumbi 3" expectedNum resultNum
         where
-            x = 2
-            y = 2
-            expectedGOLNum = 0
+            expectedNum = 0
+            preGrid = insertCells (square 3) highLife [(1,1), (1,3), (3,1), (3,3)]
+            grid = insertCells preGrid conways [(1,2), (2,1), (2,3), (3,2)]
+            resultNum = numOfDeadNeighbors (2,2) grid
+
+-----------------------------------------------------------------------------------------------------------------
+
+{--
+    Conta Vizinhos: verificamos se a função retorna a quantidade de células 
+    existentes na vizinhança de uma certa célula da grade
+--}
+
+    testNumOfLiveNeighbors0 :: Test
+    testNumOfLiveNeighbors0 = TestCase $ do
+        assertEqual "Conta Vizinhos 0" expectedNum resultNum
+        where
+            expectedNum = 8
+            grid = insertCells (square 3) conways [(1,1), (1,2),(1,3), (2,1), (2,3), (3,1), (3,2), (3,3)]
+            resultNum = numOfLiveNeighbors (2,2) grid
+
+    testNumOfLiveNeighbors1 :: Test
+    testNumOfLiveNeighbors1 = TestCase $ do
+        assertEqual "Conta Vizinhos 1" expectedNum resultNum
+        where
+            expectedNum = 0
+            resultNum = numOfLiveNeighbors (2,2) (square 3)
+
+    testNumOfLiveNeighbors2 :: Test
+    testNumOfLiveNeighbors2 = TestCase $ do
+        assertEqual "Conta Vizinhos 2" expectedNum resultNum
+        where
+            expectedNum = 4
+            grid = insertCells (square 3) conways [(1,1), (1,3), (3,1), (3,3)]
+            resultNum = numOfLiveNeighbors (2,2) grid
+
+    testNumOfLiveNeighbors3 :: Test
+    testNumOfLiveNeighbors3 = TestCase $ do
+        assertEqual "Conta Vizinhos 3" expectedNum resultNum
+        where
+            expectedNum = 4
+            grid = insertCells (square 3) conways [(1,2), (2,1), (2,3), (3,2)]
+            resultNum = numOfLiveNeighbors (2,2) grid
+
+    testNumOfLiveNeighbors4 :: Test
+    testNumOfLiveNeighbors4 = TestCase $ do
+        assertEqual "Conta Vizinhos 4" expectedNum resultNum
+        where
+            expectedNum = 0
+            resultNum = numOfLiveNeighbors (1,1) (square 1)
+
+-----------------------------------------------------------------------------------------------------------------
+
+{--
+    Simplismente Get Cell: aqui, verificamos os seus dois casos
+--}
+
+    testGetCellTrue :: Test
+    testGetCellTrue = TestCase $ do
+        assertEqual "getCellTrue" result expected
+            where
+                expected = Just conways
+                grid = insertCell (square 1) conways (1,1)
+                result = getCell (1,1) grid
+    
+    testGetCellFalse :: Test
+    testGetCellFalse = TestCase $ do 
+        assertEqual "getCellFalse" result expected
+            where
+                expected = Nothing
+                result = getCell (1,1) (square 1)
 
 -----------------------------------------------------------------------------------------------------------------
