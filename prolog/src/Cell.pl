91bfa1c92105dab0148bc60d93aea5cd441a0709
Leonardo Almeida Farias <113396827+leofarias-cpu@users.noreply.github.com>
2024-09-28 21:40:13 -0300

    Modulo de matrix

---
+++ prolog/src/Cell.pl
@@ -0,0 +1,25 @@
+:- module(cell, [])
+
+cell(dead, preto, [], []).
+% Cell é o predicado que relaciona um tipo de célula(identificada pelo nome) 
+% a suas regras e sua cor.
+createCell(Name, Color, StayRule, BirthRule) :- isValidColor(Color), 
+    assert(cell(Name, Color, StayRule, BirthRule)).
+deleteCell(Name) :- retract(cell(Name, _, _, _)).
+:- dynamic cell/4.
+
+
+% Lista os nomes de todas as células
+listCells(R) :- findall(X, cell(X, _, _, _), R).
+
+
+getCellColor(Name, Color) :- cell(Name, Color, _, _).
+getCellStay(Name, Stay) :- cell(Nome, _, Stay, _).
+getCellBirth(Name, Birth) :- cell(Nome, _, _, Birth).
+
+% esboço, esse predicado deveria relacionar a cor ao código de escape necessário
+% pra imprimir texto com aquela cor no terminal, 
+% e assim ao mesmo tempo indicar as cores válidas pro sistema.
+isValidColor(azul).
+isValidColor(vermelho).
+isValidColor(azul).
--- prolog/src/Matrix.pl
+++ prolog/src/Matrix.pl
@@ -1,2 +1,21 @@
 :- module(matrix, []).
 
+% Um dicionário fingindo ser um array, não um array de dicionários. Preenchido com Value, Size vezes.
+% Se eu não cortar aqui, ele diz que Dict = ao dicionario esperado 
+% e da mais uma possibilidade vazia, bem estranho.
+% 0 indexado, desculpa keven é mais complicado e devagar terminar em 1 e ter Tamanho valores inves de Tamanho -1
+createArrayDict(0, _, _{}):-!.
+createArrayDict(Size, Value, Dict) :-
+    N is Size - 1,
+    createArrayDict(N, Value, Partial),
+    Dict = Partial.put(N, Value).
+
+
+createMatrix(0, _, _, _{}):-!.
+createMatrix(Lines, Cols, Value, Matrix):-
+    N is Lines - 1,
+    createArrayDict(Cols, Value, Line),
+    createMatrix(N, Cols, Value, Partial),
+    Matrix = Partial.put(N, Line).
+
+createSquareMatrix(N, Value, Matrix):- createMatrix(N, N, Value, Matrix).
