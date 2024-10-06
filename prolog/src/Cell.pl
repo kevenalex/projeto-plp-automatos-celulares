81a02ea160758f919df5eb4f21821713ce871cf7
kevenalex <keven.alex.barbosa.santos@ccc.ufcg.edu.br>
2024-10-06 12:58:18 -0300

    REENVIA CELL DO MESMO JEITO QUE TA NO GITHUB

--- prolog/src/Cell.pl
+++ prolog/src/Cell.pl
@@ -1,14 +1,17 @@
 :- module(cell, []).
 
-cell(dead, preto, [], []).
+cell(dead, black, [], []).
 % Cell é o predicado que relaciona um tipo de célula(identificada pelo nome) 
 % a suas regras e sua cor.
 createCell(Name, Color, StayRule, BirthRule) :- isValidColor(Color), 
     assert(cell(Name, Color, StayRule, BirthRule)).
-% Pra quando as celulas forem lidas do json.
-createCell(cell(Name, Color, StayRule, BirthRule)) :- assert(cell(Name, Color, StayRule, BirthRule)).
 deleteCell(Name) :- retract(cell(Name, _, _, _)).
 :- dynamic cell/4.
+
+
+% Pra quando as celulas forem lidas do json.
+createCells([]).
+createCells([[N,C,S,B]|T]):- createCells(T), createCell(N, C, S, B).
 
 
 % Lista todas as células
@@ -16,12 +19,13 @@ listCells(R) :- findall(cell(X, Y, Z, W), cell(X, Y, Z, W), R).
 listCellNames(R) :- findall(X, cell(X, _, _, _), R).
 
 getCellColor(Name, Color) :- cell(Name, Color, _, _).
-getCellStay(Name, Stay) :- cell(Nome, _, Stay, _).
-getCellBirth(Name, Birth) :- cell(Nome, _, _, Birth).
+getCellStay(Name, Stay) :- cell(Name, _, Stay, _).
+getCellBirth(Name, Birth) :- cell(Name, _, _, Birth).
 
 % esboço, esse predicado deveria relacionar a cor ao código de escape necessário
 % pra imprimir texto com aquela cor no terminal, 
 % e assim ao mesmo tempo indicar as cores válidas pro sistema.
-isValidColor(azul).
-isValidColor(vermelho).
-isValidColor(azul).
+isValidColor(blue).
+isValidColor(red).
+isValidColor(green).
+isValidColor(white).
