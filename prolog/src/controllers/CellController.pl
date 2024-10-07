ceb8867afe32d7b9af70c9d0be128cb900c9aa37
Pedro Lôbo <pedro.lobo.nascimento@ccc.ufcg.edu.br>
2024-10-07 03:15:36 -0300

    começa fluxo de cellcontroller.pl

--- prolog/src/controllers/CellController.pl
+++ prolog/src/controllers/CellController.pl
@@ -4,30 +4,47 @@
 :-use_module("../src/Cell.pl").
 
 menuCells(FilePath):-
-    % Cells is cell:listCells(Cells),
-    % render:printMidScreen(Cells),
-    % read(Choice),
     read_line_to_string(user_input, Choice),
     option(Choice, FilePath).
+
+addAutomata(FilePath):-
+    render:printScreen("../app/storage/ruleController/nameCellQuestion.txt"),
+
+    render:setCursorColumn(85),
+    read_line_to_string(user_input, CellName),
+
+    addBirthRule(CellName, FilePath).
+
+addBirthRule(CellName, FilePath):-
+    render:printScreen("../app/storage/ruleController/birthRule.txt"),
     
+    render:setCursorColumn(85),
+    read_line_to_string(user_input, BirthRule),
+
+    handleBirthRule(BirthRule) ->
+        write("OMG LOBO CUIDA PAPAI")
+        
+        ;
+        render:printScreen("../app/storage/ruleController/birthRuleError.txt"),
+        sleep(0.8),
+        
+        addBirthRule(CellName, FilePath).
+
+handleBirthRule(BirthRule):-false.
+
 option("1", FilePath):- addAutomata(FilePath).
-option("2", FilePath):- addAutomata(FilePath).
-option("3", FilePath):- addAutomata(FilePath).
+option("2", FilePath):- menuCells(FilePath).
+option("3", FilePath):- menuCells(FilePath).
 option(_, FilePath):- menuCells(FilePath).
 
 teste1:-
-    cell:createCell("cu", "blue", [1], [2]),
+    % cell:createCell("teste", "blue", [1], [2]),
     % files:saveCells,
-    cell:listCellNames(R),
+    % cell:createCell("teste2", "green", [1], [2]),
+    % files:saveCells,
+    % cell:createCell("teste3", "white", [1], [2]),
+    % files:saveCells,
+    % cell:listCellNames(R),
+    % write(R),
     % cell:createCell(GameOfLife, "green", [2], [2,3]),
     menuCells("../storage/cells.json").
-
-% main:-
-%     A = _{
-%         0:_{0:a, 1:b, 2:c}, 
-%         1:_{0:d, 1:f, 2:g}, 
-%         2:_{0:h, 1:i, 2:j}
-%             },
-%     files:saveScene(cu, A),
-%     files:getSceneMatrix(cu, R),
-%     writeln(R).
