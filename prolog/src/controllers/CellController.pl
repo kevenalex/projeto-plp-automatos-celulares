8d54ed24a9faf423f7203cb1c3ed0dd5b73550d1
Pedro Lôbo <pedro.lobo.nascimento@ccc.ufcg.edu.br>
2024-10-07 19:12:47 -0300

    Finaliza CellController.pl

--- prolog/src/controllers/CellController.pl
+++ prolog/src/controllers/CellController.pl
@@ -3,33 +3,34 @@
 :-use_module("../Utils/Render.pl").
 :-use_module("../src/Cell.pl").
 
+% Prints do menu principal do CellController
 main:-
     render:clearScreen,
     render:printEmptyLines(17),
     render:printMid("CELULAS:"),
     writeln(""),
     files:getCells,
-    % render:setCursorColumn(95),
-    % files:saveCells,
-    % prints:toStringCell(Cell),
     render:listCellsPrints,
     render:printEmptyLines(2),
     render:printMid(" 1) ADICIONAR CELULA   2) DELETAR CELULA   3) VOLTAR"),
     read_line_to_string(user_input, Option),
-    option(Option, "../storage/cells.json").
+    option(Option).
 
-% Inicio do procedimento de criação da Celula, recebendo o nome da Celula e chamando a função
-% para adicionar a regra de nascimento
-addAutomata(Path):-
+% Inicio do procedimento de criação da Celula, recebendo o nome da Celula
+% e chamando a função para adicionar a regra de nascimento.
+addAutomata:-
+    render:clearScreen,
     render:printScreen("../storage/ruleController/nameCellQuestion.txt"),
 
     render:setCursorColumn(85),
     read_line_to_string(user_input, CellName),
 
-    addBirthRule(CellName, Path).
-% Criação da Regra de Nascimento da Celula, o usuario pode inserir de 0 a 8 digitos entre 1 e 8.
-% (tem que ver se os digitos validos pra criar celula ainda são esses, se não forem, alterar em isAllValidBirthDigits)
-addBirthRule(CellName, Path):-
+    addBirthRule(CellName).
+
+% Criação da Regra de Nascimento da Celula, o usuario pode inserir 
+% de 0 a 8 digitos entre 1 e 8.
+addBirthRule(CellName):-
+    render:clearScreen,
     render:printScreen("../storage/ruleController/birthRule.txt"),
     
     render:setCursorColumn(85),
@@ -37,31 +38,33 @@ addBirthRule(CellName, Path):-
 
     (handleBirthRule(BirthRule) ->
         nascList(BirthRule, BirthList),
-        addStayRule(Path, CellName, BirthList)
+        addStayRule(CellName, BirthList)
         ;
         render:printScreen("../storage/ruleController/birthRuleError.txt"),
-        sleep(0.8),
-        addBirthRule(CellName, Path)
+        sleep(2),
+        addBirthRule(CellName)
     ).
 
-% Criação da Regra de Permanencia da Celula, o usuario pode inserir de 0 a 8 digitos entre 1 e 8.
-% (tem que ver se os digitos validos pra criar celula ainda são esses, se não forem, alterar em isAllValidStayDigits)
-addStayRule(Path, CellName, BirthList):-
+% Criação da Regra de Permanencia da Celula, o usuario pode inserir de
+% 0 a 8 digitos entre 1 e 8.
+addStayRule(CellName, BirthList):-
+    render:clearScreen,
     render:printScreen("../storage/ruleController/stayRule.txt"),
 
     render:setCursorColumn(85),
     read_line_to_string(user_input, StayRule),
     (handleStayRule(StayRule) ->
         stayList(StayRule, StayList),
-        addColor(Path, CellName, BirthList, StayList)
+        addColor(CellName, BirthList, StayList)
         ;
         render:printScreen("../storage/ruleController/stayRuleError.txt"),
-        sleep(0.8),
-        addBirthRule(CellName, Path)
+        sleep(2),
+        addStayRule(CellName, BirthList)
     ).
 
-% Criação da cor da celula, tem que alterar o Color Menu que imprime na tela (ta o antigo de haskell)
-addColor(Path, CellName, BirthList, StayList):-
+% Escolha da cor da celula.
+addColor(CellName, BirthList, StayList):-
+    render:clearScreen,
     render:printScreen("../storage/ruleController/colorMenu.txt"), %tem que alterar o Color Menu
 
     render:setCursorColumn(85),
@@ -72,33 +75,34 @@ addColor(Path, CellName, BirthList, StayList):-
         main
         ;
         render:printScreen("../storage/ruleController/colorMenuError.txt"),
-        sleep(0.8),
-        addColor(Path, CellName, BirthList, StayList)
+        sleep(0.5),
+        addColor(CellName, BirthList, StayList)
     ).
 
-% menu de exclusao de celula por nome, ta funcional, mas precisa deixar bonito os prints
-% alem de que ele só considera as celulas que foram criadas em Cell.pl, e nao as que estao em cell.json
-removeAutomata(Path):-
+% Menu de exclusao de celula por nome.
+removeAutomata:-
+    render:clearScreen,
     cell:listCellNames(Cells),
     (Cells = [] ->
-        write('esta vazio'),
         main
     ;
-        render:printScreen("../storage/ruleController/listOfCells.txt"),
-        render:printEmptyLines(2),
-        render:setCursorColumn(95),
-        files:saveCells,
-        % render:printMidScreen(Celulas),
+        render:printEmptyLines(17),
+        render:printMid(" CELULAS EXISTENTES:"),
         render:printEmptyLines(1),
-        render:printScreen("../storage/ruleController/removeCellMenu.txt"),
+        render:listCellsPrints,
+        render:printEmptyLines(2),
+        render:printMid(" QUAL O NOME DA CELULA QUE VOCE QUER REMOVER?"),
         render:setCursorColumn(85),
+
         read_line_to_string(user_input, CellName),
         cell:deleteCell(CellName),
         files:saveCells,
         main
     ).
 
-% verifica se nao tem espaço em branco, se tudo é numero entre 1 e 8, se o length <= 8 e tira digitos repetidos
+% Tratamento da regra de sobrevivência, verifica se não tem espaços em branco,
+% se todos os caracteres são numeros entre 1 e 8, se o length <= 8 e remove 
+% digitos repetidos.
 handleStayRule(StayRule):-
     string_chars(StayRule, Chars),
     exclude(=( ' ' ), Chars, RegraFormatada),
@@ -106,7 +110,9 @@ handleStayRule(StayRule):-
     Length =< 8,
     isAllValidStayDigits(RegraFormatada).
 
-% verifica se nao tem espaço em branco, se tudo é numero entre 0 e 8, se o length <= 8 e tira digitos repetidos
+% Tratamento da regra de nascimento, verifica se não tem espaços em branco, 
+% se todos os caracteres são numeros entre 0 e 8, se o length <= 8 e remove 
+% digitos repetidos.
 handleBirthRule(BirthRule) :-
     string_chars(BirthRule, Chars),
     exclude(=( ' ' ), Chars, RegraFormatada),
@@ -114,41 +120,44 @@ handleBirthRule(BirthRule) :-
     Length =< 8,
     isAllValidBirthDigits(RegraFormatada).
 
-% predicado auxiliar pra verificar se o char é digito
+% Predicado auxiliar para verificar se o Char é digito.
 isDigit(Char) :-
     char_type(Char, digit).
 
-% predicado auxiliar pra transformar char em int
+% Predicado auxiliar para transformar Char em Int.
 charToInt(Char, Int) :-
     atom_number(Char, Int).
 
-% faz aquele mapeamento de 123 virar [1,2,3]
+% Faz o mapeamento para sequencia de números virarem elementos de uma lista
+% na regra de sobrevivência.
 stayList(StayRule, StayList) :-
     string_chars(StayRule, Chars),
     include(isDigit, Chars, DigitChars),
     maplist(charToInt, DigitChars, StayListNoDuplicates),
     list_to_set(StayListNoDuplicates, StayList).
 
-% faz aquele mapeamento de 123 virar [1,2,3]
+% Faz o mapeamento para sequencia de números virarem elementos de uma lista
+% na regra de nascimento.
 nascList(BirthRule, BirthList) :-
     string_chars(BirthRule, Chars),
     include(isDigit, Chars, DigitChars),
     maplist(charToInt, DigitChars, BirthListNoDuplicates),
     list_to_set(BirthListNoDuplicates, BirthList).
 
-% Verifica se todos os caracteres são números entre '1' e '8'
+% Predicado auxiliar que verifica se todos os caracteres são valores entre '1' e '8'.
 isAllValidBirthDigits([]).
 isAllValidBirthDigits([H|T]) :-
     member(H, ['1', '2', '3', '4', '5', '6', '7', '8']),
     isAllValidBirthDigits(T).
 
-% Verifica se todos os caracteres são números entre '0' e '8'
+% Predicado auxiliar que verifica se todos os caracteres são valores entre '0' e '8'.
 isAllValidStayDigits([]).
 isAllValidStayDigits([H|T]) :-
     member(H, ['0','1', '2', '3', '4', '5', '6', '7', '8']),
     isAllValidStayDigits(T).
 
-option("1", Path):- addAutomata(Path).
-option("2", Path):- removeAutomata(Path).
-option("3", Path):- !.
-option(_, Path).
+% Tratamento de erro no menu.
+option("1"):- addAutomata.
+option("2"):- removeAutomata.
+option("3"):- !.
+option(_):- main.
